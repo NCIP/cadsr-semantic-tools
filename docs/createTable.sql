8c64ab6564a054aae69a0ccb39bcd53b8f9793cc
ludetc <ludetc>
2004-09-01 19:58:05 +0000

    adds uml_loader_defaults table
    SVN-Revision: 14

---
+++ docs/createTable.sql
@@ -0,0 +1,19 @@
+DROP TABLE sbrext.UML_LOADER_DEFAULTS;
+create table sbrext.UML_LOADER_DEFAULTS (
+    id                 NUMBER   (11)                   NOT NULL
+  , context_name       VARCHAR2 (30)                   
+  , context_version    NUMBER   (4,2)                  
+  , project_name       VARCHAR2 (100)                   
+  , version            NUMBER   (4,2)                  
+  , workflow_status    VARCHAR2 (20)                   
+  , cd_version         NUMBER   (4,2)                  
+  , cd_preferred_name  VARCHAR2 (30)                   
+  , cd_conte_name      VARCHAR2 (30)                   
+);
+ALTER TABLE sbrext.UML_LOADER_DEFAULTS ADD CONSTRAINT uml_ldt_pk PRIMARY KEY
+(
+    id
+)
+NOT DEFERRABLE
+INITIALLY IMMEDIATE
+ENABLE NOVALIDATE;
