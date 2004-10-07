ab024ee859b593ae908ede8aebc37e5df74f19fe
ludetc <ludetc>
2004-10-07 15:05:03 +0000

    *** empty log message ***
    SVN-Revision: 39

--- src/gov/nih/nci/ncicb/cadsr/loader/persister/ConceptPersister.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/persister/ConceptPersister.java
@@ -11,7 +11,6 @@ import java.util.*;
 
 public class ConceptPersister extends UMLPersister {
 
-  
   private static Logger logger = Logger.getLogger(ConceptPersister.class.getName());
 
   public ConceptPersister(ElementsLists list) {
@@ -20,7 +19,22 @@ public class ConceptPersister extends UMLPersister {
   }
 
   public void persist() {
+    Concept con = DomainObjectFactory.newConcept();
+    List<Concept> cons = (List<Concept>) elements.getElements(con.getClass());
 
+    logger.debug("ConceptPersister.persist()");
+
+    if (cons != null) {
+      for(Concept c : cons) {
+        con.setPublicId(c.getPublicId());
+        List l = conceptDAO.find(con);
+        if(l == 0) { // concept does not exist: create it
+          
+        } else { // concept exist: See if we need to add alternate def.
+          
+        }
+      }
+    }
   }
 
 
