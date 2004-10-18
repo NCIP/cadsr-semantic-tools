3425d9657be8021ba43f20780fb7293b999a6a5e
ludetc <ludetc>
2004-10-18 15:03:32 +0000

    fixed automatic login --> temporary anyway
    SVN-Revision: 65

--- src/gov/nih/nci/ncicb/cadsr/loader/jaas/ConsoleCallbackHandler.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/jaas/ConsoleCallbackHandler.java
@@ -12,15 +12,17 @@ public class ConsoleCallbackHandler implements CallbackHandler {
   public void handle(Callback[] callbacks) 
     throws java.io.IOException, UnsupportedCallbackException {
 
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     for (int i = 0; i < callbacks.length; i++) {
 
       if (callbacks[i] instanceof NameCallback) {
         System.out.print(((NameCallback)callbacks[i]).getPrompt());
-        String user=(new BufferedReader(new InputStreamReader(System.in))).readLine();
+        String user=br.readLine();
         ((NameCallback)callbacks[i]).setName(user);
       } else if (callbacks[i] instanceof PasswordCallback) {
         System.out.print(((PasswordCallback)callbacks[i]).getPrompt());
-        String pass=(new BufferedReader(new InputStreamReader(System.in))).readLine();
+        String pass=br.readLine();
         ((PasswordCallback)callbacks[i]).setPassword(pass.toCharArray());
       } else {
         throw(new UnsupportedCallbackException(
