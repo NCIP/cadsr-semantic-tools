11bf5745e95bf19509aed1ded2175516fa93d5b8
anwara <anwara>
2005-03-18 21:07:49 +0000

    Added setReviewed method which changes the icon if item is reviewed.
    SVN-Revision: 231

--- src/gov/nih/nci/ncicb/cadsr/loader/ui/tree/AttributeNode.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/ui/tree/AttributeNode.java
@@ -5,18 +5,38 @@ import gov.nih.nci.ncicb.cadsr.domain.DataElement;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
-public class AttributeNode extends AbstractUMLNode {
+public class AttributeNode extends AbstractUMLNode 
+  implements ReviewableUMLNode
+  {
+  static final Icon REVIEWED_ICON = 
+    new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("tree-attribute-checked.gif"));
 
+  static final Icon DEFAULT_ICON = 
+    new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("tree-attribute.gif"));
+
+
+  private boolean reviewed = false;
+  
   public AttributeNode(DataElement de) {
     display = de.getDataElementConcept().getProperty().getLongName();
 
     fullPath = de.getDataElementConcept().getObjectClass().getLongName()
       + display;
     userObject = de;
+    
+    icon = DEFAULT_ICON;
   }
 
-  public Icon getIcon() {
-    return new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("tree-attribute.gif"));
-  }
+  public void setReviewed(boolean currentStatus) {
+    reviewed = currentStatus;
 
+    setIcon(reviewed?REVIEWED_ICON:DEFAULT_ICON);
+
+    System.out.println(icon);
+
+  }
+  
+  public boolean isReviewed() {
+    return reviewed;  
+  }
 }
