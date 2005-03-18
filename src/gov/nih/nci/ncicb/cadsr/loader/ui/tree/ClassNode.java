0c82b1dbc136c94edbc751f7e32bd3b947ba0caa
anwara <anwara>
2005-03-18 21:16:24 +0000

    Added framework to change icon when item is reviewed, but it is not implemented as of yet.
    SVN-Revision: 234

--- src/gov/nih/nci/ncicb/cadsr/loader/ui/tree/ClassNode.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/ui/tree/ClassNode.java
@@ -5,7 +5,18 @@ import gov.nih.nci.ncicb.cadsr.domain.ObjectClass;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
-public class ClassNode extends AbstractUMLNode {
+public class ClassNode extends AbstractUMLNode 
+  implements ReviewableUMLNode
+{
+
+  static final Icon REVIEWED_ICON = 
+    new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("tree-class.gif"));
+
+  static final Icon DEFAULT_ICON = 
+    new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("tree-class.gif"));
+
+  
+  private boolean reviewed = false;
 
   public ClassNode(ObjectClass oc) {
     fullPath = oc.getLongName();
@@ -13,11 +24,45 @@ public class ClassNode extends AbstractUMLNode {
     display = fullPath.substring(ind + 1);
     
     userObject = oc;
+    
+    icon = DEFAULT_ICON;
   }
 
-  public Icon getIcon() {
-    return new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("tree-class.gif"));
 
+  public void setReviewed(boolean b) 
+  {
+      this.reviewed = b;
+      
+      boolean changeIcon = true;
+      
+      // iterate over children
+      for(UMLNode l : this.getChildren())
+      {
+        if(icon != AttributeNode.REVIEWED_ICON)
+        changeIcon = false;
+      }
+      
+      if(changeIcon) 
+      {
+        this.setIcon(REVIEWED_ICON);
+      } 
+//      else
+//      {
+//        this.setIcon(REVIEWED_ICON);
+//        if() 
+//        {
+//          
+//        } else 
+//        {
+//          
+//        }
+//      }
+      
+  }
+  
+  public boolean isReviewed() 
+  {
+    return reviewed;
   }
 
 }
