8e23f48c47e77808259b2f84f6e03121f8779ade
ludetc <ludetc>
2005-03-23 19:07:26 +0000

    added popup menu
    SVN-Revision: 250

--- src/gov/nih/nci/ncicb/cadsr/loader/ui/ErrorPanel.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/ui/ErrorPanel.java
@@ -3,6 +3,7 @@ package gov.nih.nci.ncicb.cadsr.loader.ui;
 import gov.nih.nci.ncicb.cadsr.loader.ui.util.TreeUtil;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -13,10 +14,12 @@ import gov.nih.nci.ncicb.cadsr.loader.ui.tree.*;
 import java.util.*;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
-public class ErrorPanel extends JPanel {
+public class ErrorPanel extends JPanel 
+ implements MouseListener {
 
   private JTree tree;
   private Set displaySet = new HashSet();
+  private JPopupMenu popup;
 
   public ErrorPanel(UMLNode rootNode) {
     initUI(rootNode);
@@ -37,6 +40,7 @@ public class ErrorPanel extends JPanel {
     TreeUtil.expandAll(tree, new TreePath(node));
     
     tree.setCellRenderer(new UMLTreeCellRenderer());
+    tree.addMouseListener(this);
 
     this.setLayout(new BorderLayout());
 
@@ -44,12 +48,31 @@ public class ErrorPanel extends JPanel {
     this.setPreferredSize(new Dimension(450, 110));
     this.add(scrollPane, BorderLayout.CENTER);
 
+
   }
-  
+
+  public void mousePressed(MouseEvent e) {
+    showPopup(e);
+  }
+  public void mouseExited(MouseEvent e) {
+  }
+  public void mouseClicked(MouseEvent e) {
+  }
+  public void mouseEntered(MouseEvent e) {
+  }
+  public void mouseReleased(MouseEvent e) {
+    showPopup(e);
+  }
+  private void showPopup(MouseEvent e) {
+    if (e.isPopupTrigger()) {
+      popup.show(e.getComponent(),
+                 e.getX(), e.getY());
+    }
+  }
+
   public void buildPopupMenu() {
-    JPopupMenu popup = new JPopupMenu();
-    JMenuItem menuItem = new JMenuItem("A test item");
-    //menuItem.addActionListener(popup);
+    popup = new JPopupMenu();
+    JMenuItem menuItem = new JMenuItem("Export Errors");
     popup.add(menuItem);
   }
 
@@ -100,68 +123,11 @@ public class ErrorPanel extends JPanel {
 
 }
 
-//     DefaultMutableTreeNode errorNode = 
-//       new DefaultMutableTreeNode("Error");
-//     DefaultMutableTreeNode warningNode = 
-//       new DefaultMutableTreeNode("Warning");
-   
-//     rootNode.add(errorNode);
-//     rootNode.add(warningNode);
-   
-//     for(ValidationError err : errorList) {
-//       if(err.getSeverity() == errorNode.toString()) {
-// 	DefaultMutableTreeNode node = new DefaultMutableTreeNode(err.getMessage());
-// 	errorNode.add(node);
-//       }
-//       if(err.getSeverity() == warningNode.toString()) {
-// 	DefaultMutableTreeNode node = new DefaultMutableTreeNode(err.getMessage());
-// 	warningNode.add(node);
-//       }
-//     }
-//    }
-
-//   public static void main(String[] args) {
-//     JFrame runFrame = new JFrame("Test JFrame FOR ErrorPanel");
-//     runFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//     runFrame.setSize(100,100);
-
-//     JPanel errorPanel = new ErrorPanel(DummyErrorPanelData.getData());
-
-//     runFrame.getContentPane().add(BorderLayout.CENTER, errorPanel);
-//     //errorPanel.setLayout(new BorderLayout());
-//     //runFrame.add(errorPanel);
-
-//     runFrame.show();
-//   }
-
-// }
 
 class DummyErrorPanelData {
   static java.util.List<ValidationError> getData() {
 
     return new ArrayList();
 
-//     ValidationError error1 = new ValidationError("Error", "Concept Not Found");
-//     ValidationError error2 = new ValidationError("Error", "Code not Found");
-//     ValidationError error3 = new ValidationError("Error", "Element not Found");
-//     ValidationError error4 = new ValidationError("Error", "File not Found");
-      
-//     ValidationError warning1 = 
-//       new ValidationError("Warning", "Incorrect type ");
-//     ValidationError warning2 = 
-//       new ValidationError("Warning", "File not of proper type ");
-//     ValidationError warning3 = new ValidationError("Warning", "Unexpected type ");
-      
-//     java.util.List listofErrors = new ArrayList();
-//     listofErrors.add(error1);
-//     listofErrors.add(error2);
-//     listofErrors.add(error3);
-//     listofErrors.add(error4);
-      
-//     listofErrors.add(warning1);
-//     listofErrors.add(warning2);
-//     listofErrors.add(warning3);
-      
-//     return listofErrors;
   }
 }      
