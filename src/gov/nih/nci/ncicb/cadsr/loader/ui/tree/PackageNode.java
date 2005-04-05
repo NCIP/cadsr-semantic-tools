d449e0ece618af63d9c4a3c713e322b9cba72693
anwara <anwara>
2005-04-05 15:30:46 +0000

    Implements ReviewableUMLNode and checks to see whether its icon should change when all of its children have been reviewed.
    SVN-Revision: 263

--- src/gov/nih/nci/ncicb/cadsr/loader/ui/tree/PackageNode.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/ui/tree/PackageNode.java
@@ -6,19 +6,52 @@ import gov.nih.nci.ncicb.cadsr.domain.ClassSchemeClassSchemeItem;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
-public class PackageNode extends AbstractUMLNode {
+public class PackageNode extends AbstractUMLNode 
+  implements ReviewableUMLNode 
+  {
+
+  static final Icon REVIEWED_ICON = 
+    new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("tree-package-checked.gif"));
+
+  static final Icon DEFAULT_ICON = 
+    new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("tree-package.gif"));
+
+  private boolean reviewed = true;
 
   public PackageNode(String fullName, String display) {
     fullPath = fullName;
 
     this.display = display != null?
       display:fullName;
- 
+    
+    icon = DEFAULT_ICON;
   }
 
-  public Icon getIcon() {
-    return new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("tree-package.gif"));
-
+  public void setReviewed(boolean currentStatus) 
+  {    
+    boolean changeIcon = true;
+    
+    for(UMLNode l : getChildren()) 
+    {
+      ClassNode next = (ClassNode) l;
+      if(!next.getIcon().equals(ClassNode.REVIEWED_ICON)) 
+      {
+        changeIcon = false;
+        break;
+      }
+    }
+    
+    if(changeIcon)
+    {
+      setIcon(REVIEWED_ICON);
+    }
+    else
+      setIcon(DEFAULT_ICON);
+  }
+  
+  public boolean isReviewed() 
+  {
+    return reviewed;
   }
 
 }
