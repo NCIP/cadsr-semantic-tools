e8a68b3a8bc13842c88cd3f8deed098628e4aa11
ludetc <ludetc>
2005-04-25 15:49:21 +0000

    Added support for recentFiles
    SVN-Revision: 285

--- src/gov/nih/nci/ncicb/cadsr/loader/util/UserPreferences.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/util/UserPreferences.java
@@ -1,17 +1,46 @@
 package gov.nih.nci.ncicb.cadsr.loader.util;
 
 import java.util.prefs.Preferences;
+import java.util.*;
 
 public class UserPreferences {
 
+  Preferences prefs = Preferences.userRoot().node("UMLLOADER");
+
   public String getXmiDir() {
-    Preferences prefs = Preferences.userRoot().node("UMLLOADER");
     return prefs.get("xmiDir","/");
   }
 
   public void setXmiDir(String dir) {
-    Preferences prefs = Preferences.userRoot().node("UMLLOADER");
     prefs.put("xmiDir",dir);
+  }
+
+  public List<String> getRecentFiles() {
+    return new ArrayList(Arrays.asList(prefs.get("recentFiles", "").split(":")));
+
+  }
+
+  public void addRecentFile(String filePath) {
+    List<String> files = getRecentFiles();
+    
+    if(!files.contains(filePath)) {
+      if(files.size() > 4)
+        files.remove(0);
+      files.add(filePath);
+    } else {
+      if(files.size() > 4)
+        files.remove(0);
+      files.remove(filePath);
+      files.add(filePath);
+    }
+    
+    StringBuilder sb = new StringBuilder();
+    for(String s : files) {
+      if(sb.length() > 0)
+        sb.append(":");
+      sb.append(s);
+    }
+    prefs.put("recentFiles", sb.toString());
   }
 
 }
