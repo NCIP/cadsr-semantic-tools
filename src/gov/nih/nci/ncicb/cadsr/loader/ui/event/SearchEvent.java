4ff773c9c7fa0243527bcfb11ef3ae2c39f7ca94
anwara <anwara>
2005-05-04 21:15:44 +0000

    Added searchFromBottom field to determine where search should begin.
    SVN-Revision: 305

--- src/gov/nih/nci/ncicb/cadsr/loader/ui/event/SearchEvent.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/ui/event/SearchEvent.java
@@ -3,14 +3,21 @@ package gov.nih.nci.ncicb.cadsr.loader.ui.event;
 public class SearchEvent 
 {
   String searchString;
+  boolean searchFromBottom;
   
-  public SearchEvent(String find)
+  public SearchEvent(String find, boolean searchFromBottom)
   {
     searchString = find;
+    this.searchFromBottom = searchFromBottom;
   }
   
   public String getSearchString() 
   {
     return searchString;
+  }
+  
+  public boolean getSearchFromBottom() 
+  {
+    return searchFromBottom;
   }
 }
