d4092f6537e34d4e9d71f7e215002cf0c94c4d30
ludetc <ludetc>
2005-05-19 19:21:34 +0000

    concepts were read on backwards
    SVN-Revision: 354

--- src/gov/nih/nci/ncicb/cadsr/loader/ui/tree/NodeUtil.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/ui/tree/NodeUtil.java
@@ -15,11 +15,11 @@ public class NodeUtil
       String[] conceptCodes = null;
       if(node instanceof ClassNode) {
         ObjectClass oc = (ObjectClass)node.getUserObject();
-        conceptCodes = oc.getPreferredName().split("-");
+        conceptCodes = oc.getPreferredName().split(":");
         
       } else if(node instanceof AttributeNode) {
         Property prop = ((DataElement)node.getUserObject()).getDataElementConcept().getProperty();
-        conceptCodes = prop.getPreferredName().split("-");
+        conceptCodes = prop.getPreferredName().split(":");
       } 
       
       if((conceptCodes == null ) || StringUtil.isEmpty(conceptCodes[0])) {
@@ -28,8 +28,8 @@ public class NodeUtil
 
       concepts = new Concept[conceptCodes.length];
 
-      for(int i=0; i<concepts.length; 
-          concepts[i] = LookupUtil.lookupConcept(conceptCodes[i++])
+      for(int i=0, n = concepts.length; i<n; 
+          concepts[n-1-i] = LookupUtil.lookupConcept(conceptCodes[i++])
           );
       if((concepts.length > 0) && (concepts[0] == null))
         concepts = new Concept[0];
