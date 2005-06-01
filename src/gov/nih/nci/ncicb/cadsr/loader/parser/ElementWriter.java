2b5a127e4cfcd49c3d11968e10d8c60c1c0e1d5b
anwara <anwara>
2005-06-01 20:08:37 +0000

    [no comments]
    SVN-Revision: 361

--- src/gov/nih/nci/ncicb/cadsr/loader/parser/ElementWriter.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/parser/ElementWriter.java
@@ -1,8 +1,9 @@
 package gov.nih.nci.ncicb.cadsr.loader.parser;
+import gov.nih.nci.ncicb.cadsr.loader.ElementsLists;
 
 public interface ElementWriter {
 
-  public void write(ElementsList elements);
+  public void write(ElementsLists elements);
 
   public void setOutput(java.io.Writer writer);
 
