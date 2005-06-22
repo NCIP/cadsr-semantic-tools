e845cd498df5bb3d2321ba8e322f48cd6c02280d
anwara <anwara>
2005-06-22 15:27:17 +0000

    ReviewTracker is added.
    SVN-Revision: 373

---
+++ src/gov/nih/nci/ncicb/cadsr/loader/ReviewTracker.java
@@ -0,0 +1,38 @@
+package gov.nih.nci.ncicb.cadsr.loader;
+import gov.nih.nci.ncicb.cadsr.loader.event.ReviewEvent;
+import gov.nih.nci.ncicb.cadsr.loader.event.ReviewListener;
+import gov.nih.nci.ncicb.cadsr.loader.ui.tree.AbstractUMLNode;
+import gov.nih.nci.ncicb.cadsr.loader.ui.tree.ReviewableUMLNode;
+import java.util.HashMap;
+
+/**
+ * ReviewTracker keeps track of objects and their Review Status
+ * by storing them in a HashMap
+ */
+public class ReviewTracker implements ReviewListener
+{
+  private HashMap<String, Boolean> reviewed = new HashMap(); 
+  
+  public Boolean get(String key) 
+  {   
+    return reviewed.get(key);
+  }
+
+  public void put(String key, boolean value) 
+  {
+    reviewed.put(key, value);
+  }
+  
+  public void reviewChanged(ReviewEvent event) 
+  {
+    AbstractUMLNode absNode = (AbstractUMLNode) event.getUserObject();
+    if(reviewed.get(absNode.getFullPath()) != null) 
+    {
+      reviewed.remove(absNode.getFullPath());
+      this.put(absNode.getFullPath(), event.isReviewed());
+    }
+    else
+      this.put(absNode.getFullPath(), event.isReviewed());
+  }
+
+}
