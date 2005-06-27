7a863c4458efed6cec28d942eb971db85b895b20
anwara <anwara>
2005-06-27 15:58:18 +0000

    Added static variable for UML Description Preference.
    SVN-Revision: 385

--- src/gov/nih/nci/ncicb/cadsr/loader/ui/event/UserPreferencesEvent.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/ui/event/UserPreferencesEvent.java
@@ -5,6 +5,8 @@ public class UserPreferencesEvent
   public static final int VIEW_ASSOCIATION = 1;
   public static final String IN_CLASS_ASSOCIATION = "true";
   public static final String NOT_IN_CLASS_ASSOCIATION = "false";
+  public static final int UML_DESCRIPTION = 2;
+  public static final int MODE_SELECTION = 3;
   private int typeOfEvent;
   private String value;
   
