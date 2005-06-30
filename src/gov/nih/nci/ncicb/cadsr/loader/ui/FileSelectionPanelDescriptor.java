cfaedae4ed4b21c51bcd3313be2250879936b8da
anwara <anwara>
2005-06-30 17:21:14 +0000

    Disables Next button initially and then enables it if the file exists.
    SVN-Revision: 411

--- src/gov/nih/nci/ncicb/cadsr/loader/ui/FileSelectionPanelDescriptor.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/ui/FileSelectionPanelDescriptor.java
@@ -2,6 +2,7 @@ package gov.nih.nci.ncicb.cadsr.loader.ui;
 
 import java.awt.event.*;
 import java.io.*;
+import gov.nih.nci.ncicb.cadsr.loader.ui.event.*;
 
 public class FileSelectionPanelDescriptor 
   extends WizardPanelDescriptor
@@ -17,16 +18,29 @@ public class FileSelectionPanelDescriptor
 
     nextPanelDescriptor = ProgressFileSelectionPanelDescriptor.IDENTIFIER;
     backPanelDescriptor = ModeSelectionPanelDescriptor.IDENTIFIER;
+    
+   
+  }
+
+  public void aboutToDisplayPanel() 
+  {
+    setNextButtonAccordingToSelection();
   }
 
   public void init() {
     panel.init();
+    panel.addActionListener(this);
   }
   
   public void actionPerformed(ActionEvent evt) {
+    setNextButtonAccordingToSelection();
+  }
+    
+  private void setNextButtonAccordingToSelection() 
+  {
     String path = panel.getSelection();
     File f = new File(path);
     getWizardModel().setNextButtonEnabled(new Boolean(f.exists()));
+ 
   }
-    
 }
