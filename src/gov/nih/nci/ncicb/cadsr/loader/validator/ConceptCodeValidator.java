81a64d8052226a9e405770db0ca438fa8f6e5b2b
ludetc <ludetc>
2005-07-05 18:55:58 +0000

    fixes validation of OCs and Props
    SVN-Revision: 428

--- src/gov/nih/nci/ncicb/cadsr/loader/validator/ConceptCodeValidator.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/validator/ConceptCodeValidator.java
@@ -12,6 +12,8 @@ public class ConceptCodeValidator implements Validator {
 
   private ElementsLists elements;
 
+  private ValidationItems items = ValidationItems.getInstance();
+
   public ConceptCodeValidator(ElementsLists elements) {
     this.elements = elements;
   }
@@ -20,58 +22,60 @@ public class ConceptCodeValidator implements Validator {
    * returns a list of Validation errors.
    */
   public ValidationItems validate() {
-    ValidationItems items = ValidationItems.getInstance();
     
-//     List conceptErrors = (List)elements.getElements(ConceptError.class);
-//     if(conceptErrors != null) 
-//       for(Iterator it = conceptErrors.iterator(); it.hasNext(); ) {
-//         ConceptError o = (ConceptError)it.next();
-//         errors.add(o);
-//       }
-
-    ObjectClass oc = DomainObjectFactory.newObjectClass();
-    List ocs = (List)elements.getElements(oc.getClass());
+    List<ObjectClass> ocs = (List<ObjectClass>)elements.getElements(DomainObjectFactory.newObjectClass().getClass());
     if(ocs != null)
-      for(Iterator it = ocs.iterator(); it.hasNext(); ) {
-        ObjectClass o = (ObjectClass)it.next();
-        if(o.getPreferredName() == null || o.getPreferredName().length() < 4) {
+      for(ObjectClass o : ocs) {
+        if(StringUtil.isEmpty(o.getPreferredName()))
           items.addItem(new ValidationError("Class: " + o.getLongName() + " has no concept code.", o));
+        else {
+          checkConcepts(o);
         }
-      }
+      }    
 
-    Property prop = DomainObjectFactory.newProperty();
-    List props = elements.getElements(prop.getClass());
+
+    List<Property> props = (List<Property>)elements.getElements(DomainObjectFactory.newProperty().getClass());
     if(props != null)
-      for(Iterator it = props.iterator(); it.hasNext(); ) {
-        Property o = (Property)it.next();
-        if(o.getPreferredName() == null || o.getPreferredName().length() < 4) {
+      for(Property o : props ) 
+        if(StringUtil.isEmpty(o.getPreferredName()))
           items.addItem(new ValidationError("Attribute: " + o.getLongName() + " has no concept code.", o));
+        else {
+          checkConcepts(o);
         }
-      }
+    
+//     List<Concept> concepts = (List<Concept>)elements.getElements(DomainObjectFactory.newConcept().getClass());
+//     if(concepts != null) {
+//       for(Concept o : concepts ) {
+//         Concept o = (Concept)it.next();
+//         if(o.getPreferredName() == null) {
 
-
-    Concept con = DomainObjectFactory.newConcept();
-    List concepts = elements.getElements(con.getClass());
-    if(concepts != null) {
-      for(Iterator it = concepts.iterator(); it.hasNext(); ) {
-        Concept o = (Concept)it.next();
-        if(o.getPreferredName() == null) {
-          // capture this above. Dont need any more.
-//           errors.add(new ValidationError(SEVERITY_ERROR, 
-//                                          PropertyAccessor.getProperty("validation.concept.missing")));
-        } else {
-          if(o.getLongName() == null)
-            items.addItem(new ValidationError(PropertyAccessor.getProperty("validation.concept.missing.longName", o.getPreferredName()), o));
-          if(o.getPreferredDefinition() == null) {
-            items.addItem(new ValidationError(PropertyAccessor.getProperty("validation.concept.missing.definition", o.getPreferredName()), o));
-          }
-          if(o.getDefinitionSource() == null)
-            items.addItem(new ValidationError(PropertyAccessor.getProperty("validation.concept.missing.source", o.getPreferredName()), o));
-        }
-      }
-    }
+//         } else {
+//           if(o.getLongName() == null)
+//             items.addItem(new ValidationError(PropertyAccessor.getProperty("validation.concept.missing.longName", o.getPreferredName()), o));
+//           if(o.getPreferredDefinition() == null) {
+//             items.addItem(new ValidationError(PropertyAccessor.getProperty("validation.concept.missing.definition", o.getPreferredName()), o));
+//           }
+//           if(o.getDefinitionSource() == null)
+//             items.addItem(new ValidationError(PropertyAccessor.getProperty("validation.concept.missing.source", o.getPreferredName()), o));
+//         }
+//       }
+//     }
 
     return items;
+  }
+
+  private void checkConcepts(AdminComponent ac) {
+    String[] conStr = ac.getPreferredName().split(":");
+    for(String s : conStr) {
+      Concept con = LookupUtil.lookupConcept(s);
+      if(con.getLongName() == null)
+        items.addItem(new ValidationError(PropertyAccessor.getProperty("validation.concept.missing.longName", con.getPreferredName()), ac));
+      if(con.getPreferredDefinition() == null) {
+        items.addItem(new ValidationError(PropertyAccessor.getProperty("validation.concept.missing.definition", con.getPreferredName()), ac));
+      }
+      if(con.getDefinitionSource() == null)
+        items.addItem(new ValidationError(PropertyAccessor.getProperty("validation.concept.missing.source", con.getPreferredName()), ac));
+    }
   }
 
 }
