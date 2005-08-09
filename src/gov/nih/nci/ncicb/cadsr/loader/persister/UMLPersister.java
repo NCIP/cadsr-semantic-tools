654a53f9143a12a922120ffe0b6045e1be750af3
ludetc <ludetc>
2005-08-09 18:27:23 +0000

    added mapping DateTime --> java.util.Date
    SVN-Revision: 507

--- src/gov/nih/nci/ncicb/cadsr/loader/persister/UMLPersister.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/persister/UMLPersister.java
@@ -56,6 +56,7 @@ public class UMLPersister implements Persister {
     vdMapping.put("String", "java.lang.String");
 
     vdMapping.put("Date", "java.util.Date");
+    vdMapping.put("DateTime", "java.util.Date");
 
     vdMapping.put("any", "java.lang.Object");
     vdMapping.put("valueany", "java.lang.Object");
