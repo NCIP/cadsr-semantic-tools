68847799e56a4e9914a80913d933f415d76d629e
anwara <anwara>
2006-01-19 22:03:47 +0000

    Added new DescriptionValidator that issues a warning if no Description is included.
    SVN-Revision: 791

---
+++ src/gov/nih/nci/ncicb/cadsr/loader/validator/DescriptionValidator.java
@@ -0,0 +1,48 @@
+package gov.nih.nci.ncicb.cadsr.loader.validator;
+import gov.nih.nci.ncicb.cadsr.domain.*;
+import gov.nih.nci.ncicb.cadsr.loader.ElementsLists;
+import gov.nih.nci.ncicb.cadsr.loader.event.ProgressListener;
+import java.util.*;
+import gov.nih.nci.ncicb.cadsr.loader.util.*;
+
+public class DescriptionValidator implements Validator
+{
+  private ElementsLists elements;
+  
+  private ValidationItems items = ValidationItems.getInstance();
+  
+  public DescriptionValidator(ElementsLists elements)
+  {
+    this.elements = elements;
+  }
+  
+  public void addProgressListener(ProgressListener l) {
+
+  }
+  
+  public ValidationItems validate() 
+  {
+    List<ObjectClass> ocs = (List<ObjectClass>)elements.getElements(DomainObjectFactory.newObjectClass().getClass());
+    if(ocs != null) {   
+      for(ObjectClass oc : ocs) {  
+        if(StringUtil.isEmpty(oc.getPreferredDefinition()))
+          items.addItem(new ValidationWarning("Class: " + oc.getLongName() + " has no description.", oc));
+      }
+    }
+    boolean found = false;
+    List<DataElement> des = (List<DataElement>)elements.getElements(DomainObjectFactory.newDataElement().getClass());
+    if(des != null) {  
+      for(DataElement de : des) {  
+        for(AlternateName an : de.getAlternateNames()) {
+          if(an.getType() == AlternateName.TYPE_UML_DE) 
+            found = true;
+            break;
+        }
+      if(!found)
+        items.addItem(new ValidationWarning("Attribute " + de.getDataElementConcept().getLongName() + " has no description.", de));
+      }
+    }
+    
+    return items; 
+    }
+}
