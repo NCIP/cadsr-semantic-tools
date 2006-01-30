85914075f2f7cc11ee80c382b54dc126a25271fb
ludetc <ludetc>
2006-01-30 16:20:14 +0000

    shows log messages
    SVN-Revision: 822

--- src/gov/nih/nci/ncicb/cadsr/loader/ui/LogPanel.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/ui/LogPanel.java
@@ -3,6 +3,12 @@ package gov.nih.nci.ncicb.cadsr.loader.ui;
 import javax.swing.*;
 import javax.swing.event.*;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Checkbox;
+
+import java.awt.event.*;
+
 import java.util.*;
 
 import org.apache.log4j.spi.*;
@@ -15,35 +21,99 @@ public class LogPanel extends JPanel
 
   private LogPanel _this = this;
 
-  public void append(LoggingEvent loggingEvent) {
-    events.add(loggingEvent);
-    _this.updateUI();
-  }
+  private JCheckBox debugCb = new JCheckBox("Debug", false),
+    infoCb = new JCheckBox("Info", true),
+    warnCb = new JCheckBox("Warnings", true),
+    errorCb = new JCheckBox("Errors", true);
 
   private List<LoggingEvent> events = new ArrayList<LoggingEvent>();
+  private List<LoggingEvent> displayEvents =
+    new ArrayList<LoggingEvent>();
+
+  private JList list;
+
+  public void append(LoggingEvent loggingEvent) {
+    events.add(loggingEvent);
+    if(includeInDisplay(loggingEvent))
+      displayEvents.add(loggingEvent);
+    _this.updateUI();
+  }
 
   public LogPanel() {
     initUI();
   }
 
+  private boolean includeInDisplay(LoggingEvent evt) {
+    if(evt.getLevel().equals(Level.DEBUG)) {
+      return debugCb.isSelected();
+    } else if(evt.getLevel().equals(Level.INFO)) {
+      return infoCb.isSelected();
+    } else if(evt.getLevel().equals(Level.WARN)) {
+      return warnCb.isSelected();
+    } else if(evt.getLevel().equals(Level.ERROR)) {
+      return errorCb.isSelected();
+    }
+    return true;
+  }
+
+  private void rebuildDisplay() {
+    displayEvents.clear();
+
+    for(LoggingEvent evt : events) {
+      if(includeInDisplay(evt))
+        displayEvents.add(evt);
+     
+    }
+    list.updateUI();
+  }
   
   private void initUI() {
-    JList list = new JList(new ListModel() {
+    setLayout(new BorderLayout());
+
+    list = new JList(new ListModel() {
         public void addListDataListener(ListDataListener l) {
 
         }
         public Object getElementAt(int index) {
-          return events.get(index).getMessage();
+          return displayEvents.get(index).getMessage();
         }
         public int getSize() {
-          return events.size();
+          return displayEvents.size();
         }
         public void removeListDataListener(ListDataListener l) {
           
         }
       });
 
-    add(list);
+    JScrollPane scrollPane = new JScrollPane(list);
+    add(scrollPane, BorderLayout.CENTER);
+
+    JPanel cbPanel = new JPanel();
+    cbPanel.setLayout(new GridLayout(4, 1));
+
+    ActionListener cbAl = new ActionListener() {
+      public void actionPerformed(ActionEvent evt) {
+        rebuildDisplay();
+      }
+    };
+
+    cbPanel.add(debugCb);
+    cbPanel.add(infoCb);
+    cbPanel.add(warnCb);
+    cbPanel.add(errorCb);
+
+    infoCb.setSelected(true);
+    warnCb.setSelected(true);
+    errorCb.setSelected(true);
+
+    debugCb.addActionListener(cbAl);
+    infoCb.addActionListener(cbAl);
+    warnCb.addActionListener(cbAl);
+    errorCb.addActionListener(cbAl);
+
+    add(cbPanel, BorderLayout.EAST);
+
+
   }
 
 }
