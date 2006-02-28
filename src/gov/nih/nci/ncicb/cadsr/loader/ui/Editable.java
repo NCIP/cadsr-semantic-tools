c6e10a0aafef6df161befd940c08de4d22a9057b
no_author <no_author@no_author.cvs2svn>
2006-02-28 20:57:56 +0000

    This commit was manufactured by cvs2svn to create tag 'umlclassdiagramloader_1_0_0_0_trunk_deploy-stage_build_23'.
    SVN-Revision: 919

--- src/gov/nih/nci/ncicb/cadsr/loader/ui/Editable.java
+++
@@ -1,8 +0,0 @@
-package gov.nih.nci.ncicb.cadsr.loader.ui;
-
-public interface Editable 
-{
-
-  public void applyPressed();
-
-}
--- test/test1/sql/associations.sql
+++
@@ -1,6 +0,0 @@
-select recs.long_name, recs.direction, source.long_name, target.long_name, 
- source_low_multiplicity "srcLow",  source_high_multiplicity "srcHi", target_low_multiplicity "tarLow", target_high_multiplicity "tarHi", rl_name type
-from oc_recs_ext recs, object_classes_ext source, object_classes_ext target
-where t_oc_idseq = target.oc_idseq
-and s_oc_idseq = source.oc_idseq
-and recs.created_by = 'LADINO';
