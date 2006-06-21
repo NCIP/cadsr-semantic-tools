be103d90ba025f7a3040d313d148edcd27eccd43
anwara <anwara>
2006-06-21 15:32:57 +0000

    InheritedAttributeNode.
    SVN-Revision: 1032

---
+++ src/gov/nih/nci/ncicb/cadsr/loader/ui/tree/InheritedAttributeNode.java
@@ -0,0 +1,12 @@
+package gov.nih.nci.ncicb.cadsr.loader.ui.tree;
+import gov.nih.nci.ncicb.cadsr.domain.DataElement;
+
+public class InheritedAttributeNode extends AttributeNode 
+{
+
+    public InheritedAttributeNode(DataElement de) 
+    {
+      super(de);
+    }
+
+}
