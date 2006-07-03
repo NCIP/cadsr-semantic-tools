3c94c05cae0af2551bf25d93fa5528c204356bcb
anwara <anwara>
2006-07-03 19:39:18 +0000

    Enlarged PreferenceDialog.
    SVN-Revision: 1054

--- src/gov/nih/nci/ncicb/cadsr/loader/ui/PreferenceDialog.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/ui/PreferenceDialog.java
@@ -68,7 +68,7 @@ public class PreferenceDialog extends JDialog implements ActionListener
     this.getContentPane().setLayout(new BorderLayout());
     this.getContentPane().add(centerPanel);
     this.getContentPane().add(southPanel,BorderLayout.SOUTH);
-    this.setSize(300,200);
+    this.setSize(300,225);
     
     apply.setActionCommand(APPLY);
     cancel.setActionCommand(CANCEL);
