50ced301c318785a0ac7aaba5442970c8cf713c2
ludetc <ludetc>
2007-05-29 16:09:40 +0000

    don't check for tv.getName().matches(tag + separator + "[2345678]?"))
    SVN-Revision: 1498

--- src/gov/nih/nci/ncicb/cadsr/loader/parser/XMIParser2.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/parser/XMIParser2.java
@@ -936,12 +936,12 @@ public class XMIParser2 implements Parser {
 
     // now check that we don't have a misused tagged value.
     // tag + 
-    Collection<UMLTaggedValue> taggedValues = elt.getTaggedValues();
-    for(UMLTaggedValue _tv : taggedValues) {
-      if(_tv.getName().startsWith(tag + separator))
-        if(!_tv.getName().matches(tag + separator + "[2345678]?"))
-          throw new ParserException(PropertyAccessor.getProperty("invalid.tagged.value", _tv.getName()));
-    }
+//     Collection<UMLTaggedValue> taggedValues = elt.getTaggedValues();
+//     for(UMLTaggedValue _tv : taggedValues) {
+//       if(_tv.getName().startsWith(tag + separator))
+//         if(!_tv.getName().matches(tag + separator + "[2345678]?"))
+//           throw new ParserException(PropertyAccessor.getProperty("invalid.tagged.value", _tv.getName()));
+//     }
 
   }
 
