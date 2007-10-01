6a3371cb274017f09087df042730951223de7809
ludetc <ludetc>
2007-10-01 17:32:19 +0000

    fixed 9199. Was checking parentDE == inheritedDE instead of comparing properties...
    SVN-Revision: 1646

--- src/gov/nih/nci/ncicb/cadsr/loader/validator/InheritanceValidator.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/validator/InheritanceValidator.java
@@ -52,9 +52,16 @@ public class InheritanceValidator implements Validator, CadsrModuleListener {
       if(inheritedDE.getPublicId() != null) {
         DataElement parentDE = inheritedList.getParent(inheritedDE);
         while(parentDE != null) {
-          if(parentDE.getPublicId() != null) {
-            if(!inheritedDE.getPublicId().equals(parentDE.getPublicId())
-               || !inheritedDE.getVersion().equals(parentDE.getVersion())) {
+          if(parentDE.getPublicId() != null && 
+             parentDE.getDataElementConcept() != null &&
+             parentDE.getDataElementConcept().getProperty() != null && 
+             inheritedDE.getDataElementConcept() != null &&
+             inheritedDE.getDataElementConcept().getProperty() != null 
+             ) {
+            Property inheritedProp = inheritedDE.getDataElementConcept().getProperty();
+            Property parentProp = parentDE.getDataElementConcept().getProperty();
+            if(!inheritedProp.getPublicId().equals(parentProp.getPublicId())
+               || !inheritedProp.getVersion().equals(parentProp.getVersion())) {
               items.addItem(new ValidationError
                             (PropertyAccessor.getProperty
                              ("inherited.and.parent.property.mismatch", LookupUtil.lookupFullName(inheritedDE), LookupUtil.lookupFullName(parentDE)),inheritedDE));
