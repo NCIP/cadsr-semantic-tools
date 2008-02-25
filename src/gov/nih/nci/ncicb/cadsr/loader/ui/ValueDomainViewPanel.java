1199ab24c670e134fc44901b097d8dc3088894c6
bhardwajs <bhardwajs>
2008-02-25 20:51:38 +0000

    Changes made for VD preferred definition. GForge item # 12396. - Sharad
    SVN-Revision: 1871

--- src/gov/nih/nci/ncicb/cadsr/loader/ui/ValueDomainViewPanel.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/ui/ValueDomainViewPanel.java
@@ -21,6 +21,8 @@ import javax.swing.*;
 
 import gov.nih.nci.ncicb.cadsr.loader.ui.util.UIUtil;
 
+import gov.nih.nci.ncicb.cadsr.loader.util.ConventionUtil;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -251,8 +253,7 @@ public class ValueDomainViewPanel extends JPanel
           vdTypeNRadioButton.setSelected(false);
         }
 
-    vdCDPublicIdJLabel.setText(tempVD.getConceptualDomain().getPublicId() +
-      " / " + tempVD.getConceptualDomain().getVersion());
+    vdCDPublicIdJLabel.setText(ConventionUtil.publicIdVersion(tempVD.getConceptualDomain()));
 
     if(tempVD.getConceptualDomain().getLongName() != null
         && !tempVD.getConceptualDomain().getLongName().equals(""))
@@ -260,8 +261,7 @@ public class ValueDomainViewPanel extends JPanel
     else
       vdCdLongNameValueJLabel.setText("Unable to lookup CD Long Name");
 
-    vdRepIdValueJLabel.setText(tempVD.getRepresentation().getPublicId() +
-      " / " + tempVD.getRepresentation().getVersion());
+    vdRepIdValueJLabel.setText(ConventionUtil.publicIdVersion(tempVD.getRepresentation()));
 
     firePropertyChangeEvent(new PropertyChangeEvent(this, ApplyButtonPanel.SAVE, null, false));
     vdPrefDefValueTextField.getDocument().addDocumentListener(this);
@@ -272,8 +272,7 @@ public class ValueDomainViewPanel extends JPanel
   }
   
   private void setVdCdSearchedValues(){
-      vdCDPublicIdJLabel.setText(tempVD.getConceptualDomain().getPublicId() +
-        " / " + tempVD.getConceptualDomain().getVersion());
+      vdCDPublicIdJLabel.setText(ConventionUtil.publicIdVersion(tempVD.getConceptualDomain()));
 
       if(tempVD.getConceptualDomain().getLongName() != null
           && !tempVD.getConceptualDomain().getLongName().equals(""))
@@ -283,8 +282,7 @@ public class ValueDomainViewPanel extends JPanel
   }
   
   private void setRepSearchedValues(){
-      vdRepIdValueJLabel.setText(tempVD.getRepresentation().getPublicId() +
-        " / " + tempVD.getRepresentation().getVersion());
+      vdRepIdValueJLabel.setText(ConventionUtil.publicIdVersion(tempVD.getRepresentation()));
   }
   
   public static void main(String args[]) 
@@ -299,27 +297,8 @@ public class ValueDomainViewPanel extends JPanel
 
     public void applyPressed() {   
         
-        conceptualDomain = tempVD.getConceptualDomain();
-        String tmpCDPublicId = vdCDPublicIdJLabel.getText().substring(0, (vdCDPublicIdJLabel.getText().indexOf(" / ")));
-        String tmpCDVersion = vdCDPublicIdJLabel.getText().substring(vdCDPublicIdJLabel.getText().indexOf(" / ")+3, vdCDPublicIdJLabel.getText().length());
-        String tmpCDLongName = vdCdLongNameValueJLabel.getText();
-        if(!tmpCDPublicId.equals("null"))
-            conceptualDomain.setPublicId(tmpCDPublicId);
-        if(!tmpCDVersion.equals("null"))
-            conceptualDomain.setVersion(new Float(tmpCDVersion));
-        if(!tmpCDVersion.equals("null"))       
-            conceptualDomain.setLongName(tmpCDLongName);
-
-        representation = tempVD.getRepresentation();
-        String tmpRepPublicId = String.valueOf(vdRepIdValueJLabel.getText().substring(0, (vdRepIdValueJLabel.getText().indexOf(" / "))));
-        String tmpRepVersion = String.valueOf(vdRepIdValueJLabel.getText().substring((vdRepIdValueJLabel.getText().indexOf(" / ")+3), vdRepIdValueJLabel.getText().length()));
-        if(!tmpRepPublicId.equals("null"))
-            representation.setPublicId(tmpRepPublicId);
-        if(!tmpRepVersion.equals("null"))
-            representation.setVersion(new Float(tmpRepVersion));
-        
-        vd.setConceptualDomain(conceptualDomain);
-        vd.setRepresentation(representation);
+        vd.setConceptualDomain(tempVD.getConceptualDomain());
+        vd.setRepresentation(tempVD.getRepresentation());
         if(vdDatatypeValueCombobox.getSelectedIndex() != 0){
             vd.setDataType(String.valueOf(vdDatatypeValueCombobox.getSelectedItem()));}
         if(vdPrefDefValueTextField.getText() != null || !vdPrefDefValueTextField.getText().equals("null") || vdPrefDefValueTextField.getText().length() > 0)
@@ -330,7 +309,6 @@ public class ValueDomainViewPanel extends JPanel
             vd.setVdType("N");
         vdTypeERadioButton.addItemListener(this);
         vdTypeNRadioButton.addItemListener(this);
-                
         
         firePropertyChangeEvent(new PropertyChangeEvent(this, ApplyButtonPanel.SAVE, null, false));
         fireElementChangeEvent(new ElementChangeEvent(umlNode));
