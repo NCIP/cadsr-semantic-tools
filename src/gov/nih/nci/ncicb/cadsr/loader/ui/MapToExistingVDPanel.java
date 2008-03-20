a0777f946e28ea469d728a0bc960a942824847a1
bhardwajs <bhardwajs>
2008-03-20 16:17:57 +0000

    Changes made for LVD/caDSR comparison and addition for VD Long Name: Sharad.
    SVN-Revision: 1944

--- src/gov/nih/nci/ncicb/cadsr/loader/ui/MapToExistingVDPanel.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/ui/MapToExistingVDPanel.java
@@ -1,41 +1,43 @@
 package gov.nih.nci.ncicb.cadsr.loader.ui;
 
 import gov.nih.nci.ncicb.cadsr.domain.DomainObjectFactory;
+import gov.nih.nci.ncicb.cadsr.domain.PermissibleValue;
 import gov.nih.nci.ncicb.cadsr.domain.ValueDomain;
 import gov.nih.nci.ncicb.cadsr.loader.event.ElementChangeEvent;
 import gov.nih.nci.ncicb.cadsr.loader.event.ElementChangeListener;
 import gov.nih.nci.ncicb.cadsr.loader.ext.CadsrModule;
 import gov.nih.nci.ncicb.cadsr.loader.ext.CadsrModuleListener;
 import gov.nih.nci.ncicb.cadsr.loader.ui.tree.UMLNode;
-
-
-import java.awt.*;
-
-import javax.swing.*;
-
 import gov.nih.nci.ncicb.cadsr.loader.ui.util.UIUtil;
-
 import gov.nih.nci.ncicb.cadsr.loader.util.ConventionUtil;
-
 import gov.nih.nci.ncicb.cadsr.loader.util.StringUtil;
 
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 import java.beans.PropertyChangeEvent;
-
 import java.beans.PropertyChangeListener;
-
 import java.text.DateFormat;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.log4j.Logger;
 
 public class MapToExistingVDPanel extends JPanel 
     implements Editable, CadsrModuleListener
 {
   private JLabel vdPrefDefTitleLabel = new JLabel("VD Preferred Definition"),
+    vdLongNameLabel = new JLabel("VD Long Name"),
     vdDatatypeTitleLabel = new JLabel("VD Datatype"),
     vdCdIdTitleLabel = new JLabel("VD CD PublicId / Version"),
     vdRepIdTitleLabel = new JLabel("Representation Term"),
@@ -43,6 +45,7 @@ public class MapToExistingVDPanel extends JPanel
     vdCreatedByLabel = new JLabel("Created By"),
     vdCreatedDateLabel = new JLabel("Created Date");
   private JLabel vdDatatypeTitleLabelValue = new JLabel(),
+    vdLongNameLabelValue = new JLabel(),
     vdTypeTitleLabelValue = new JLabel(),
     vdCdIdTitleLabelValue = new JLabel(),
     vdRepIdTitleLabelValue = new JLabel(),
@@ -57,12 +60,14 @@ public class MapToExistingVDPanel extends JPanel
 
   private ValueDomain vd, tempVD;
   private JButton searchButton;
+  private JButton lvdCadsrButton;
   private UMLNode umlNode;
   private CadsrModule cadsrModule;
 
   private boolean isInitialized = false;
   private List<ElementChangeListener> changeListeners = new ArrayList<ElementChangeListener>();
   private List<PropertyChangeListener> propChangeListeners = new ArrayList<PropertyChangeListener>();  
+  private Logger logger = Logger.getLogger(MapToExistingVDPanel.class.getName());
 
   
   public void addPropertyChangeListener(PropertyChangeListener l) {
@@ -85,6 +90,7 @@ public class MapToExistingVDPanel extends JPanel
     tempVD.setConceptualDomain(vd.getConceptualDomain());
     tempVD.setPreferredDefinition(vd.getPreferredDefinition());
     tempVD.setRepresentation(vd.getRepresentation());
+    tempVD.setLongName(vd.getLongName());
     tempVD.setDataType(vd.getDataType());
     tempVD.setVdType(vd.getVdType());
     tempVD.setPublicId(vd.getPublicId());
@@ -114,24 +120,26 @@ public class MapToExistingVDPanel extends JPanel
     UIUtil.insertInBag(mainPanel, vdPrefDefTitleLabel, 0, 1);
     UIUtil.insertInBag(mainPanel, scrollPane, 1, 1, 3, 1); 
     
-    UIUtil.insertInBag(mainPanel, vdDatatypeTitleLabel, 0, 3);
-    UIUtil.insertInBag(mainPanel, vdDatatypeTitleLabelValue, 1, 3);
+    UIUtil.insertInBag(mainPanel, vdLongNameLabel, 0, 3);
+    UIUtil.insertInBag(mainPanel, vdLongNameLabelValue, 1, 3);
+
+    UIUtil.insertInBag(mainPanel, vdDatatypeTitleLabel, 0, 5);
+    UIUtil.insertInBag(mainPanel, vdDatatypeTitleLabelValue, 1, 5);
     
-    UIUtil.insertInBag(mainPanel, vdCdIdTitleLabel, 0, 5);
-    UIUtil.insertInBag(mainPanel, vdCdIdTitleLabelValue, 1, 5);
+    UIUtil.insertInBag(mainPanel, vdCdIdTitleLabel, 0, 6);
+    UIUtil.insertInBag(mainPanel, vdCdIdTitleLabelValue, 1, 6);
     
-    UIUtil.insertInBag(mainPanel, vdCdLongNameTitleLabel, 0, 6);
-    UIUtil.insertInBag(mainPanel, vdCdLongNameTitleLabelValue, 1, 6);
+    UIUtil.insertInBag(mainPanel, vdCdLongNameTitleLabel, 0, 7);
+    UIUtil.insertInBag(mainPanel, vdCdLongNameTitleLabelValue, 1, 7);
 
-    UIUtil.insertInBag(mainPanel, vdRepIdTitleLabel, 0, 7);
-    UIUtil.insertInBag(mainPanel, vdRepIdTitleLabelValue, 1, 7);
+    UIUtil.insertInBag(mainPanel, vdRepIdTitleLabel, 0, 8);
+    UIUtil.insertInBag(mainPanel, vdRepIdTitleLabelValue, 1, 8);
 
+    UIUtil.insertInBag(mainPanel, vdCreatedByLabel, 0, 9);
+    UIUtil.insertInBag(mainPanel, vdCreatedByLabelValue, 1, 9);
 
-    UIUtil.insertInBag(mainPanel, vdCreatedByLabel, 0, 8);
-    UIUtil.insertInBag(mainPanel, vdCreatedByLabelValue, 1, 8);
-
-    UIUtil.insertInBag(mainPanel, vdCreatedDateLabel, 0, 9);
-    UIUtil.insertInBag(mainPanel, vdCreatedDateLabelValue, 1, 9);
+    UIUtil.insertInBag(mainPanel, vdCreatedDateLabel, 0, 10);
+    UIUtil.insertInBag(mainPanel, vdCreatedDateLabelValue, 1, 10);
 
     JScrollPane scrollPane = new JScrollPane(mainPanel);
     scrollPane.getVerticalScrollBar().setUnitIncrement(30);
@@ -145,9 +153,24 @@ public class MapToExistingVDPanel extends JPanel
         setSearchedValues();
         firePropertyChangeEvent(new PropertyChangeEvent(this, ApplyButtonPanel.SAVE, null, true));
     }});
+
+    lvdCadsrButton = new JButton("Display Permissible Values");
+    lvdCadsrButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent evt) {
+            try {
+                List<PermissibleValue> cadsrPVs = cadsrModule.getPermissibleValues(vd);
+                List<PermissibleValue> localPVs = vd.getPermissibleValues();
+                PVCompareDialog pvCompareDialog = new PVCompareDialog(localPVs, cadsrPVs);
+                pvCompareDialog.setVisible(true);
+                pvCompareDialog.setAlwaysOnTop(true);
+            } catch (Exception e) {
+                logger.error(e);
+            } 
+    }});
     JPanel searchButtonPanel = new JPanel();
     searchButtonPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); 
     searchButtonPanel.add(searchButton);
+    searchButtonPanel.add(lvdCadsrButton);
 
     this.add(scrollPane, BorderLayout.CENTER);
     this.add(searchButtonPanel, BorderLayout.SOUTH);
@@ -159,6 +182,7 @@ public class MapToExistingVDPanel extends JPanel
   
     if(StringUtil.isEmpty(tempVD.getPublicId())) {
         vdPrefDefValueTextField.setText("");
+        vdLongNameLabelValue.setText("");
         vdDatatypeTitleLabelValue.setText("");
         vdTypeTitleLabelValue.setText("");
         vdCdIdTitleLabelValue.setText("");
@@ -168,6 +192,7 @@ public class MapToExistingVDPanel extends JPanel
         vdCreatedDateLabelValue.setText("");
     } else {
         vdPrefDefValueTextField.setText(tempVD.getPreferredDefinition());
+        vdLongNameLabelValue.setText(tempVD.getLongName());
         vdDatatypeTitleLabelValue.setText(tempVD.getDataType());
         vdTypeTitleLabelValue.setText(tempVD.getVdType());
         vdCdIdTitleLabelValue.setText(ConventionUtil.publicIdVersion(tempVD.getConceptualDomain()));
@@ -187,6 +212,7 @@ public class MapToExistingVDPanel extends JPanel
         }
         if(tempVD.getRepresentation() != null)
             vdRepIdTitleLabelValue.setText(ConventionUtil.publicIdVersion(tempVD.getRepresentation()));
+        vdLongNameLabelValue.setText(tempVD.getLongName());
         vdDatatypeTitleLabelValue.setText(tempVD.getDataType());
         vdTypeTitleLabelValue.setText(tempVD.getVdType());
         vdCreatedByLabelValue.setText(tempVD.getAudit().getCreatedBy());
@@ -206,12 +232,14 @@ public class MapToExistingVDPanel extends JPanel
     public void applyPressed() {   
         vd.setConceptualDomain(tempVD.getConceptualDomain());
         vd.setRepresentation(tempVD.getRepresentation());
+        vd.setLongName(tempVD.getLongName());
         vd.setDataType(tempVD.getDataType());
         vd.setPreferredDefinition(tempVD.getPreferredDefinition());
         vd.setVdType(tempVD.getVdType());
         vd.setPublicId(tempVD.getPublicId());
         vd.setVersion(tempVD.getVersion());
         vd.setAudit(tempVD.getAudit());
+        vd.setId(tempVD.getId());
         
         firePropertyChangeEvent(new PropertyChangeEvent(this, ApplyButtonPanel.SAVE, null, false));
         fireElementChangeEvent(new ElementChangeEvent(umlNode));
