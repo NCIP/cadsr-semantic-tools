c640edee878dd2f8ce47eee0b621502ca75c1e6e
ludetc <ludetc>
2008-05-28 18:37:57 +0000

    poplate VD result can be null
    SVN-Revision: 2038

--- src/gov/nih/nci/ncicb/cadsr/loader/event/UMLDefaultHandler.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/event/UMLDefaultHandler.java
@@ -632,7 +632,7 @@ public class UMLDefaultHandler
         logger.error("Could not query cadsr module ", e);
       } // end of try-catch
 
-      if(result.size() == 0) {
+      if(result == null || result.size() == 0) {
         ChangeTracker changeTracker = ChangeTracker.getInstance();
 
         ValidationError item = new ValidationError(PropertyAccessor.getProperty("vd.doesnt.exist", attributeName,
