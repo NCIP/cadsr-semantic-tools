3d0d1e86c5d84b1d44446481a4e1ccb6ca13ab5e
ludetc <ludetc>
2008-07-07 19:16:57 +0000

    better report of bad VD ID / Version mapping
    SVN-Revision: 2129

--- src/gov/nih/nci/ncicb/cadsr/loader/event/UMLDefaultHandler.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/event/UMLDefaultHandler.java
@@ -142,7 +142,12 @@ public class UMLDefaultHandler
         
         vd.setPublicId(null);
         vd.setVersion(null);
+
+        vd.setLongName(event.getName());
+        vd.setConceptualDomain(DomainObjectFactory.newConceptualDomain());
+        vd.setRepresentation(DomainObjectFactory.newRepresentation());
 //         changeTracker.put
+
 //           (className + "." + attributeName, 
 //            true);
       } else {
