86cf293d6f3286d1162dcef4ab4e26e68b302643
ludetc <ludetc>
2008-12-15 19:14:50 +0000

    extracted runmethod. make generic
    SVN-Revision: 2307

--- src/gov/nih/nci/ncicb/cadsr/loader/UMLLoader.java
+++ src/gov/nih/nci/ncicb/cadsr/loader/UMLLoader.java
@@ -83,7 +83,6 @@ public class UMLLoader {
    * @exception Exception if an error occurs
    */
   public static void main(String[] args) throws Exception {
-
     if(args.length != 3) {
       System.err.println(PropertyAccessor.getProperty("usage"));
       System.exit(1);
@@ -97,7 +96,7 @@ public class UMLLoader {
       System.exit(1);
     }
 
-    UMLLoader loader = BeansAccessor.getUmlLoader();
+    CadsrLoader loader = BeansAccessor.getUmlLoader();
 
     String ignoreVd = (String)(System.getProperties().get("ignore-vd"));
     if((ignoreVd != null) & (ignoreVd.equals("true"))) {
@@ -111,173 +110,5 @@ public class UMLLoader {
 
   }
 
-  private void run(String fileDir, String projectName, Float projectVersion) throws Exception {
-    prefs.setUsePrivateApi(true);
-
-    InitClass initClass = new InitClass(this);
-    Thread t = new Thread(initClass);
-    t.setPriority(Thread.MAX_PRIORITY);
-    t.start();
-
-//     try {
-//       Thread.currentThread().sleep(500);
-//     } catch (Exception e){
-//     } // end of try-catch
-
-    UserSelections userSelections = UserSelections.getInstance();
-    RunMode mode = RunMode.Loader;
-    userSelections.setProperty("MODE", mode);
-    userSelections.setProperty("SKIP_VD_VALIDATION", true);
-    
-    String[] filenames = new File(fileDir).list(new FilenameFilter() {
-	public boolean accept(File dir, String name) {
-	  return name.endsWith(".xmi") || name.endsWith(".uml");
-	}
-      });
-
-    if(filenames == null) 
-      filenames = new String[0];
-    
-    if(filenames.length == 0) {
-      logger.info(PropertyAccessor.getProperty("no.files"));
-      System.exit(0);
-    }
-
-    SwingCallbackHandler sch = new SwingCallbackHandler();
-    LoginContext lc = new LoginContext("UML_Loader", sch);
-
-    sch.dispose();
-
-    String username = null;
-    
-    try {
-      lc.login();
-      boolean loginSuccess = true;
-      
-      Subject subject = lc.getSubject();
-
-      Iterator it = subject.getPrincipals().iterator();
-      while (it.hasNext()) {
-	username = it.next().toString();
-	logger.debug(PropertyAccessor.getProperty("authenticated", username));
-      }
-    } catch (Exception ex) {
-      logger.error(PropertyAccessor.getProperty("login.fail",ex.getMessage()));
-      System.exit(1);
-    }
-    
-    
-    logger.info(PropertyAccessor.getProperty("nbOfFiles", filenames.length));
-
-    synchronized(initClass) {
-      if(!initClass.isDone())
-        try {
-          wait();
-        } catch (Exception e){
-        } // end of try-catch
-    }
-
-
-    fireNewRunMode(mode);
-    
-    for(int i=0; i<filenames.length; i++) {
-      logger.info(PropertyAccessor.getProperty("startingFile", filenames[i]));
-
-      UMLDefaults defaults = UMLDefaults.getInstance();
-      try {
-        defaults.initParams(projectName, projectVersion, username);
-      } catch (Exception e) {
-        logger.error(e);
-        System.exit(1);
-      } // end of try-catch
-//       defaults.initClassifications();
-      defaults.initWithDB();
-      defaults.setUsername(username);
-
-      parser.parse(fileDir + "/" + filenames[i]);
-      
-    }
-
-    ValidationItems items = validator.validate();
-    Set<ValidationError> errors = items.getErrors();
-    if(errors.size() > 0) {
-      for(ValidationError error : errors) {
-        logger.error(error.getMessage());
-      }
-      System.exit(1);
-    }
-
-    Set<ValidationWarning> warnings = items.getWarnings();
-    if(warnings.size() > 0) {
-      // Ask user if we should continue
-      for(ValidationWarning warning : warnings) {
-        logger.warn(warning.getMessage());
-      }
-      String answ = JOptionPane.showInputDialog(PropertyAccessor.getProperty("validation.continue"));
-      if(answ == null || !answ.equals("y")) {
-        System.exit(1);
-      }
-    }
-
-    ProgressFrame progressFrame = new ProgressFrame(100);
-    progressFrame.setVisible(true);
-    
-    persister.setProgressListener(progressFrame);
-
-    persister.persist();
-
-    logger.info("refreshing database views");
-    
-    // Refresh the views
-    Connection conn = null;
-    CallableStatement cs = null;
-    try {
-        conn = dataSource.getConnection();
-        cs = conn.prepareCall("{call sbrext_admin_mv.refresh_mvw}");
-        cs.execute();
-    }
-    catch (SQLException e) {
-        e.printStackTrace();
-    }
-    finally {
-        if (conn != null) {
-            conn.close();
-        }
-    }
-
-    logger.info("refreshed databased views");
-    
-    progressFrame.dispose();
-
-    System.exit(0);
-  }
-
-  public void setValidator(Validator validator) {
-    this.validator = validator;
-  }
-
-  public void setParser(Parser parser) {
-    this.parser = parser;
-  }
-
-  public void setPersister(Persister persister) {
-    this.persister = persister;
-  }
-
-  public void setUserPreferences(UserPreferences preferences) {
-    prefs = preferences;
-  }
-
-  public void setDataSource(DataSource dataSource) {
-    this.dataSource = dataSource;
-  }
-  public void setRunModeListeners(java.util.List<RunModeListener> listeners) {
-    this.runModeListeners = listeners;
-  }
-
-  private void fireNewRunMode(RunMode runMode) {
-    for(RunModeListener l : runModeListeners)
-      l.setRunMode(runMode);
-  }
 
 }
